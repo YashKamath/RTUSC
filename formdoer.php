1f7ef4ca78396f02a9bae58e1369dc01f69aa4db
YashKamath <ykamath@usc.edu>
2015-11-19 22:32:47 -0800

    Update formdoer.php

--- formdoer.php
+++ formdoer.php
@@ -1,5 +1,14 @@
 <?php
-if($_POST["message"]) {
-    mail("reviewteamusc@gmail.com", "Form to email message", $_POST["message"], "From: reviewteamusc@gmail.com");
-}
+    $to = "mail@yourdomain.com"; 
+    $from = $_REQUEST['email']; 
+    $name = $_REQUEST['name']; 
+    $headers = "From: $from"; 
+    $subject = "You have a message sent from your site"; 
+    $fields = array(); 
+    $fields{"name"} = "name"; 
+    $fields{"email"} = "email"; 
+    $fields{"phone"} = "phone"; 
+    $fields{"message"} = "message";
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $send = mail($to, $subject, $body, $headers);
 ?>
